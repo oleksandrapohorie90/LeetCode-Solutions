94fadb4fc74e6f5a9d9f45d6fdf2495aecc8ac0b
Alex Pohorielova | SDET <114103716+oleksandrapohorie90@users.noreply.github.com>
2025-09-10 22:14:31 -0400

    Time: 2 ms (97.77%), Space: 43.4 MB (66.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
+    if (s.length() != t.length()) return false;
 
-        char[] a = s.toCharArray();
-        char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a, b);
+    int[] counts = new int[26]; // for 'a' to 'z'
+
+    for (char c : s.toCharArray()) {
+        counts[c - 'a']++; // increment for s
     }
+
+    for (char c : t.toCharArray()) {
+        counts[c - 'a']--; // decrement for t
+        if (counts[c - 'a'] < 0) {
+            return false; // more of this char in t than in s
+        }
+    }
+
+    return true; // all counts balanced
+}
+
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length())
+    //         return false;
+
+    //     char[] a = s.toCharArray();
+    //     char[] b = t.toCharArray();
+    //     Arrays.sort(a);
+    //     Arrays.sort(b);
+    //     return Arrays.equals(a, b);
+    // }
 }
